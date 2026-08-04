9070ea690746b06934762bfeff5e27ee10d5394b
Z User <z@container>
2026-08-04 22:57:26 +0000

    fix: WhatsApp number +15406704172 unified across all components

--- src/components/bttsbet/MobileTabBar.tsx
+++ src/components/bttsbet/MobileTabBar.tsx
@@ -47,7 +47,7 @@ const TABS: { id: TabId; label: string; icon: React.ReactNode; action: () => voi
         <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
       </svg>
     ),
-    action: () => window.open('https://wa.me/221781234567', '_blank'),
+    action: () => window.open('https://wa.me/15406704172', '_blank'),
   },
 ]
 
